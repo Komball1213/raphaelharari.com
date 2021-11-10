120ffcb5904e02fec5ab2634da34352365b3c9f0
Komball1213 <leonard.veuthey@hotmail.com>
2021-11-10 12:01:25 +0100

    10/11/21

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const cursor = document.querySelector(".cursor");
+// const cursor = document.querySelector(".cursor");
 
-document.addEventListener("mousemove", e => {
+// document.addEventListener("mousemove", e => {
 
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+e.pageX+"px; ")
-})
+//     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+e.pageX+"px; ")
+// })
+
+// ANIM                 ----
+$( document ).ready(function() {
+    $("#screen").delay(4000).fadeOut(3000, function(){
+        $("#screen").hide()
+    });
+
+    $(".home").fadeOut().delay(5000).fadeIn(3000);
+});
+
+// CTA                 -----
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelectorAll(".cta-wrapper").classList.add("fadeIn");
+        }
+    })
+});
+
+observer.observe(document.querySelectorAll(".cta"));
